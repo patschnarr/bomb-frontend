91776a05a6768c7a8364a5360898b183fb0e1a21
Aaron Shames <aaron@1coin.io>
2021-11-22 20:38:25 -0500

    icons

--- src/components/TokenSymbol/TokenSymbol.tsx
+++ src/components/TokenSymbol/TokenSymbol.tsx
@@ -1,19 +1,20 @@
 import React from 'react';
 
 //Graveyard ecosystem logos
-import bombLogo from '../../assets/img/crypto_tomb_cash.svg';
-import tShareLogo from '../../assets/img/crypto_tomb_share.svg';
-import bombLogoPNG from '../../assets/img/crypto_tomb_cash.f2b44ef4.png';
-import tShareLogoPNG from '../../assets/img/crypto_tomb_share.bf1a6c52.png';
-import tBondLogo from '../../assets/img/crypto_tomb_bond.svg';
+import bombLogo from '../../assets/img/bomb-256.png';
+import tShareLogo from '../../assets/img/bshare-256.png';
+import bombLogoPNG from '../../assets/img/bomb-512.png';
+import tShareLogoPNG from '../../assets/img/bshare-512.png';
+import tBondLogo from '../../assets/img/bbond-256.png';
 
-import bombFtmLpLogo from '../../assets/img/tomb_ftm_lp.png';
-import bshareFtmLpLogo from '../../assets/img/tshare_ftm_lp.png';
+import bombFtmLpLogo from '../../assets/img/bomb-btc-lp-512.png';
+import bshareFtmLpLogo from '../../assets/img/bshare-bnb-lp-512.png';
 
-import wftmLogo from '../../assets/img/ftm_logo_blue.svg';
-import booLogo from '../../assets/img/spooky.png';
-import zooLogo from '../../assets/img/zoo_logo.svg';
-import shibaLogo from '../../assets/img/shiba_logo.svg';
+import bnbLogo from '../../assets/img/bnb.png';
+import wftmLogo from '../../assets/img/bnb.png';
+import booLogo from '../../assets/img/bnb.png';
+import zooLogo from '../../assets/img/bnb.png';
+import shibaLogo from '../../assets/img/bnb.png';
 
 const logosBySymbol: { [title: string]: string } = {
   //Real tokens
@@ -23,16 +24,16 @@ const logosBySymbol: { [title: string]: string } = {
   BSHAREPNG: tShareLogoPNG,
   BSHARE: tShareLogo,
   BBOND: tBondLogo,
-  WBNB: wftmLogo,
-  BOO: booLogo,
-  SHIBA: shibaLogo,
-  ZOO: zooLogo,
-  CAKE: bombLogo,
-  SUSD: bombLogo,
-  SBTC: bombLogo,
-  BTCB: bombLogo,
-  BTC: bombLogo,
-  SVL: bombLogo,
+  WBNB: bnbLogo,
+  BOO: bnbLogo,
+  SHIBA: bnbLogo,
+  ZOO: bnbLogo,
+  CAKE: bnbLogo,
+  SUSD: bnbLogo,
+  SBTC: bnbLogo,
+  BTCB: bnbLogo,
+  BTC: bnbLogo,
+  SVL: bnbLogo,
   'BOMB-BNB-LP': bombFtmLpLogo,
   'BOMB-BTCB-LP': bombFtmLpLogo,
   'BSHARE-BNB-LP': bshareFtmLpLogo,
